5930392f4e27062ca5c4fc8663d7a1dc50084055
RudrayyaSwamu <rudrayyaswamy248@gmail.com>
2024-01-26 15:01:21 +0530

    first commit

---
+++ src/main/java/org/example/chatgptservice/ChatGptServiceApplication.java
@@ -0,0 +1,14 @@
+package org.example.chatgptservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ChatGptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptServiceApplication.class, args);
+	}
+
+}
